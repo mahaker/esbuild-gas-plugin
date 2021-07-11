adc250fd623bb13e497faef6c98a32250cccd240
takker99 <37929109+takker99@users.noreply.github.com>
2021-07-11 14:03:32 +0900

    :bug: separate a type definition

--- mod.ts
+++ mod.ts
@@ -1,9 +1,6 @@
 import type { PluginBuild } from "https://deno.land/x/esbuild@v0.12.15/mod.d.ts";
+// @deno-types="./generate.d.ts"
 import { generate } from "https://esm.sh/gas-entry-generator@2.1.0";
-
-declare function generate(outfile: string, options?: { comment?: boolean }): {
-  entryPointFunctions: string;
-};
 
 export default {
   name: "gas-plugin",
