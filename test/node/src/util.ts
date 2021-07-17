3e39b8b6a7ff5f10279bd7406b48bfbfdbc5bbf9
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2021-07-17 22:46:06 +0900

    setup esbuild and codes, build inside ava

---
+++ test/node/src/util.ts
@@ -0,0 +1,7 @@
+const add = (n1: number, n2: number): number => n1 + n2
+const sub = (n1: number, n2: number): number => n1 - n2
+
+export default {
+  add,
+  sub
+}
--- test/node/test.js
+++ test/node/test.js
@@ -1,5 +1,21 @@
 const test = require('ava');
+const fs = require('fs')
+const esbuild = require('esbuild')
+const { GasPlugin } = require('../../')
 
-test('my passing test', t => {
+test.beforeEach(() => {
+  fs.rmdirSync('./dist', { recursive: true })
+})
+
+test('declare global functions', async t => {
+  await esbuild.build({
+    entryPoints: ['src/main.ts'],
+    bundle: true,
+    logLevel: 'info',
+    outfile: 'dist/bundle.js',
+    target: ['node14'],
+    plugins: [GasPlugin]
+  })
+
   t.is(1, 1)
 });
