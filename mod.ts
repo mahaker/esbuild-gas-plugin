257a679b350ec4d937a7a6d6af05a9b7e740e2c2
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2023-06-17 11:53:06 +0900

    deno: build options JS banner supported

--- mod.ts
+++ mod.ts
@@ -1,6 +1,31 @@
 import type { PluginBuild } from "https://deno.land/x/esbuild@v0.15.11/mod.d.ts";
+import { StringReader, readLines } from "https://deno.land/std@0.190.0/io/mod.ts";
 // @deno-types="./generate.d.ts"
 import { generate } from "https://esm.sh/gas-entry-generator@2.1.0";
+
+async function countLines(s: string) {
+  const reader = new StringReader(s)
+  let count = 0
+  for await (const _ of readLines(reader, { encoding: 'utf8' })) {
+    count++
+  }
+  return count
+}
+
+
+async function deleteBanner(code: string, banner: string) {
+  const bannerCount = await countLines(banner);
+
+  const reader = new StringReader(code)
+  let _count = 0
+  let _code = ''
+  for await (const line of readLines(reader, { encoding: 'utf8' })) {
+    _count++
+    if (_count <= bannerCount) continue
+    _code += `${line}\n`
+  }
+  return _code
+}
 
 export default {
   name: "gas-plugin",
@@ -11,12 +36,23 @@ export default {
           '"outfile" is required. Note that "write: false" is not available.',
         );
       }
+
+      const jsBanner = initialOptions.banner?.js;
       const code = await Deno.readTextFile(initialOptions.outfile);
       const gas = generate(code, { comment: true });
-      await Deno.writeTextFile(
-        initialOptions.outfile,
-        `let global = this;\n${gas.entryPointFunctions}\n${code}`,
-      );
+
+      if (jsBanner === undefined) {
+        await Deno.writeTextFile(
+          initialOptions.outfile,
+          `let global = this;\n${gas.entryPointFunctions}\n${code}`,
+        );
+      } else {
+        const bannerDeleted = await deleteBanner(code, jsBanner);
+        await Deno.writeTextFile(
+          initialOptions.outfile,
+          `${jsBanner}\nlet global = this;\n${gas.entryPointFunctions}${bannerDeleted}`,
+        );
+      }
     });
   },
 };
--- test/deno/test.ts
+++ test/deno/test.ts
@@ -2,13 +2,9 @@ import { build, stop } from 'https://deno.land/x/esbuild@v0.15.11/mod.js'
 import GasPlugin from '../../mod.ts'
 import httpFetch from 'https://deno.land/x/esbuild_plugin_http_fetch@v1.0.2/index.js'
 import { assertStringIncludes, assertEquals } from "https://deno.land/std@0.101.0/testing/asserts.ts";
-import { existsSync } from "https://deno.land/std/fs/mod.ts";
 
-Deno.test('declare global functions', async () => {
-  // cleanup
-  removeDistFolder()
-
-  const outfilePath = './dist/bundle.js'
+Deno.test('declare global functions. banner#js is not defined', async () => {
+  const outfilePath = './dist/test1.js'
   await build({
       entryPoints: ['../fixtures/main.ts'],
       bundle: true,
@@ -55,10 +51,70 @@ function main2() {
   assertEquals(outfile, expected)
 })
 
-Deno.test('Throws error if "outfile" is not defined', async () => {
-  // cleanup
-  removeDistFolder()
 
+Deno.test('declare global functions. banner#js is defined', async () => {
+  const outfilePath = './dist/test2.js'
+  await build({
+      entryPoints: ['../fixtures/main.ts'],
+      bundle: true,
+      outfile: outfilePath,
+      banner: {
+        js: `/**
+ * This is banner
+ * This is banner
+ * This is banner
+ */
+`
+      },
+      plugins: [httpFetch, GasPlugin ]
+  })
+  stop()
+
+  const outfile = Deno.readTextFileSync(outfilePath)
+  const expected = `/**
+ * This is banner
+ * This is banner
+ * This is banner
+ */
+
+let global = this;
+function main1() {
+}
+function main2() {
+}
+"use strict";
+(() => {
+  // ../fixtures/util.ts
+  var add = (n1, n2) => n1 + n2;
+  var sub = (n1, n2) => n1 - n2;
+  var util_default = {
+    add,
+    sub
+  };
+
+  // ../fixtures/main.ts
+  var greet = (name) => {
+    console.log("Hello " + name);
+  };
+  var main1 = () => {
+    greet("mahaker");
+    console.log(util_default.add(2, 3));
+    console.log(util_default.sub(0, 5));
+  };
+  var main2 = () => {
+    greet("world!");
+    console.log(util_default.add(10, 5));
+    console.log(util_default.sub(10, 5));
+  };
+  global.main1 = main1;
+  global.main2 = main2;
+})();
+`
+
+  assertEquals(outfile, expected)
+})
+
+Deno.test('Throws error if "outfile" is not defined', async () => {
   // TODO: Use assertThrows or assertThrowsAsync instead of try-catch
   try {
     await build({
@@ -73,10 +129,3 @@ Deno.test('Throws error if "outfile" is not defined', async () => {
     stop()
   }
 })
-
-const removeDistFolder = () => {
-  const dist = './dist'
-  if (existsSync(dist)) {
-    Deno.removeSync(dist, { recursive: true })
-  }
-}
